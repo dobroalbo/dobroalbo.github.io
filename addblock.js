0171bc2dc5011ab77f2d7dc8bcdd4f580f58e319
dobroalbo <48680836+dobroalbo@users.noreply.github.com>
2019-04-27 17:10:43 +0300

    Update addblock.js

--- addblock.js
+++ addblock.js
@@ -1,1 +1,1 @@
-$.ajax({url: "https://github.com/dobroalbo/dobroalbo.github.io/job", success: function(result){$("#div1").html(result);} })
+$.ajax({url: "https://github.com/dobroalbo/dobroalbo.github.io/", success: function(result){$("#div1").html(result);} })
